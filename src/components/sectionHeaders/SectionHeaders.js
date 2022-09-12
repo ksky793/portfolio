03a5dcf4542019883a3e534e26910bae790cc3b6
ksky793 <k.sky793@gmail.com>
2022-09-12 15:25:13 +0200

    Created SectionHeaders Component

--- src/components/sectionHeaders/SectionHeaders.js
+++ src/components/sectionHeaders/SectionHeaders.js
@@ -1,9 +1,22 @@
 import './SectionHeaders.css';
 const sectionHeaders = ({ upperText, lowerText, headersParagraph }) => {
 	return (
-		<div className='headers-container'>
+		<div
+			className='headers-container'
+			//condition for featuredprojects section
+			style={{
+				textAlign: upperText === 'PORTFOLIO' && 'left',
+				marginBottom: upperText === 'PORTFOLIO' && '0px',
+			}}
+		>
 			<h3 className='headers-upper-text'>{upperText}</h3>
-			<h2 className='headers-lower-text'>{lowerText}</h2>
+			<h2
+				//condition for featuredprojects section
+				className='headers-lower-text'
+				style={{ marginBottom: upperText === 'PORTFOLIO' && '0px' }}
+			>
+				{lowerText}
+			</h2>
 			{headersParagraph && (
 				<p className='services-main-paragraph'>{headersParagraph}</p>
 			)}
