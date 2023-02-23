bc5dc9a7b4c00b3a4fcba1c51f2db46fba369c15
ksky793 <k.sky793@gmail.com>
2023-02-23 02:41:14 +0100

    Refactoring sekcji Skills / Zastosowanie metodologii BEM

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,4 +1,4 @@
-import './Skills.css';
+import './Skills.scss';
 import SectionHeaders from '../sectionHeaders/SectionHeaders';
 import Skill from './skill/Skill';
 const Skills = () => {
@@ -24,39 +24,23 @@ const Skills = () => {
 				'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png',
 		},
 		{
-			skillName: 'TYPESCRIPT',
-			image:
-				'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Typescript.svg/1920px-Typescript.svg.png',
-		},
-		{
-			skillName: 'SQL',
-			image:
-				'https://upload.wikimedia.org/wikipedia/en/thumb/6/68/Oracle_SQL_Developer_logo.svg/800px-Oracle_SQL_Developer_logo.svg.png',
+			skillName: 'REDUX',
+			image: 'https://cdn.iconscout.com/icon/free/png-256/redux-283024.png',
 		},
 		{
 			skillName: 'ADOBE XD',
 			image:
 				'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Adobe_XD_CC_icon.svg/800px-Adobe_XD_CC_icon.svg.png',
 		},
-		{
-			skillName: 'GIT',
-			image: 'https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png',
-		},
-		{
-			skillName: 'VS CODE',
-			image:
-				'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/1200px-Visual_Studio_Code_1.35_icon.svg.png',
-		},
 	];
 	return (
-		<section className='section-skills' id='skills'>
+		<section className='skills' id='skills'>
 			<div data-aos='fade-up' className='wrapper skills-container'>
 				<SectionHeaders
 					upperText='What Skills I Have'
 					lowerText='My Tech Stack'
 				/>
-
-				<div className='skills-icons'>
+				<div className='skills__icons'>
 					{experiences.map((skill, index) => (
 						<Skill
 							key={index}
