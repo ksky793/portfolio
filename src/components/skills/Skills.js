a469bbfb7d0b4c921015381d694539a0a998d523
ksky793 <k.sky793@gmail.com>
2023-02-23 02:55:53 +0100

    Dodano dodatkową umiejętność

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -27,6 +27,12 @@ const Skills = () => {
 			skillName: 'REDUX',
 			image: 'https://cdn.iconscout.com/icon/free/png-256/redux-283024.png',
 		},
+
+		{
+			skillName: 'SASS',
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/1280px-Sass_Logo_Color.svg.png',
+		},
 		{
 			skillName: 'ADOBE XD',
 			image:
