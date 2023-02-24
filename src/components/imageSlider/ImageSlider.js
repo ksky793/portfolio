be7a81975849299e27f5076dabc96efb032b0fb0
ksky793 <k.sky793@gmail.com>
2023-02-25 00:46:32 +0100

    Refactoring sekcji projects / zastosowanie metodyki bem / stworzono ImageSlider

---
+++ src/components/imageSlider/ImageSlider.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react';
+import './ImageSlider.scss';
+import { AiOutlineLeft, AiOutlineRight } from 'react-icons/ai';
+import { GoPrimitiveDot } from 'react-icons/go';
+const ImageSlider = ({ slides }) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+
+	const goToPrevious = () => {
+		if (currentIndex === 0) {
+			setCurrentIndex(slides.length - 1);
+		} else setCurrentIndex((currentIndex) => currentIndex - 1);
+	};
+
+	const goToNext = () => {
+		if (currentIndex === slides.length - 1) {
+			setCurrentIndex(0);
+		} else setCurrentIndex((currentIndex) => currentIndex + 1);
+	};
+
+	const goToSlide = (slideIndex) => {
+		setCurrentIndex(slideIndex);
+	};
+
+	useEffect(() => {
+		// Ustawienie intervalu
+		const slideInterval = setInterval(() => {
+			goToNext();
+		}, 5000); // zmień wartość czasu według własnych potrzeb
+
+		// Czyszczenie intervalu, aby uniknąć wycieku pamięci
+		return () => {
+			clearInterval(slideInterval);
+		};
+	}, [currentIndex]);
+
+	return (
+		<div className='slider'>
+			<div className='slider__left-arrow' onClick={goToPrevious}>
+				<AiOutlineLeft />
+			</div>
+			<div className='slider__right-arrow' onClick={goToNext}>
+				<AiOutlineRight />
+			</div>
+			<div
+				className='slider__slide'
+				style={{ backgroundImage: `url(${slides[currentIndex].image})` }}
+			></div>
+			<div className='slider__dots'>
+				{slides.map((slide, index) => (
+					<GoPrimitiveDot
+						className={`dot ${index === currentIndex ? 'dot--active' : ''}`}
+						key={index}
+						onClick={() => goToSlide(index)}
+					/>
+				))}
+			</div>
+			<div className='slider__caption'>
+				<h4 className='slider__caption__name'>{slides[currentIndex].name}</h4>
+
+				<div className='slider__caption__buttons m-t--20'>
+					<a
+						href={slides[currentIndex].websiteUrl}
+						className='btns btn-demo'
+						target='_blank'
+						rel='noreferrer'
+					>
+						DEMO
+					</a>
+
+					<a
+						href={slides[currentIndex].githubUrl}
+						className='btns btn-github'
+						target='_blank'
+						rel='noreferrer'
+					>
+						GITHUB
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default ImageSlider;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,15 +1,56 @@
-import './Projects.css';
+import './Projects.scss';
 import SectionHeaders from '../sectionHeaders/SectionHeaders';
-import Project from './project/project/Project';
+import ImageSlider from '../imageSlider/ImageSlider';
 
+// images
+import portfolio from '../../assets/images/portfolio.png';
+import ecommerce from '../../assets/images/ecommerce.png';
+import netflix from '../../assets/images/netflix.png';
+import hotels from '../../assets/images/hotels.png';
+import notatnik from '../../assets/images/notatnik.png';
+
+const projects = [
+	{
+		name: 'Portfolio',
+		image: portfolio,
+		websiteUrl: 'https://portfolio-e13cf.web.app',
+		githubUrl: 'https://github.com/ksky793/portfolio',
+	},
+	{
+		name: 'Ecommerce (in progress)',
+		image: ecommerce,
+		websiteUrl: 'https://github.com/ksky793/ecommerce-react-redux-firebase',
+		githubUrl: 'https://github.com/ksky793/ecommerce-react-redux-firebase',
+	},
+	{
+		name: 'Hotels',
+		image: hotels,
+		websiteUrl: 'https://noclegi-5b7a2.web.app',
+		githubUrl: 'https://github.com/ksky793/noclegi-app',
+	},
+	{
+		name: 'Netflix Clone',
+		image: netflix,
+		websiteUrl: 'https://netflix-clone-62010.web.app',
+		githubUrl: 'https://github.com/ksky793/netflix',
+	},
+	{
+		name: 'CRUD Notes',
+		image: notatnik,
+		websiteUrl: 'https://notes-ef74d.web.app',
+		githubUrl: 'https://github.com/ksky793/notatnik',
+	},
+];
 const Projects = () => {
 	return (
-		<section className='section-featured-projects' id='portfolio'>
-			<div data-aos='fade-up' className='wrapper featured-projects-container'>
-				<div className='featured-projects-header-container'>
+		<section className='portfolio' id='portfolio'>
+			<div className='wrapper portfolio__container'>
+				<div className='portfolio__header'>
 					<SectionHeaders upperText='PORTFOLIO' lowerText='My Projects' />
 				</div>
-				<Project />
+				<div className='portfolio__slider'>
+					<ImageSlider slides={projects} />
+				</div>
 			</div>
 		</section>
 	);
